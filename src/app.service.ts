0d9f38c8775d7da9b19a673d732b551cef454a49
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2025-02-20 09:33:31 +0100

    first test in App Module

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUsers() {
+    return [
+      {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@doe.com',
+        password: '123456',
+      },
+    ];
+  }
 }
